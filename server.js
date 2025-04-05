47e77d22d0a4c36bf883ba8aeebca779091f143e
Venkatesh01777 <bv.22u10666@btech.nitdgp.ac.in>
2025-04-05 22:12:33 +0530

    v5 hosting

--- server.js
+++ server.js
@@ -8,13 +8,12 @@ const app = express();
 const port = 3000;
 dotenv.config();
 
-// PostgreSQL connection
+// PostgreSQL connection for neon db
 const pool = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Neon SSL
+  },
 });
 
 // Test database connection
